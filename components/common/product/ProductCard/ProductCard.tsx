d2e456f53038d79b9960db5362e2111bf7f3c398
Brendan Milton <brendan.milton@valiant.finance>
2023-11-27 06:29:27 +1100

    Added Product Card and sample image to display for products.

--- components/common/product/ProductCard/ProductCard.tsx
+++ components/common/product/ProductCard/ProductCard.tsx
@@ -1,17 +1,39 @@
 
 import { Product } from '@/framework/common/types/product';
 import { FunctionComponent } from 'react';
+import Link from "next/link"
+import Image from "next/image"
 
 interface Props {
     product: Product
 }
 
+const placeholderImage = '/product-image-placeholder.svg'
 const ProductCard: FunctionComponent<Props> = ({product}) => {
 
     return (
-        <div>
-            {product.name}
-        </div>
+      <Link legacyBehavior href={`/products/${product.slug}`}>
+        <a>            
+          <div>
+            <h3>
+              <span>{product.name}</span>
+            </h3>
+            <span>14 $</span>
+          </div>
+          {
+            product.images && product.images.length > 0 && (
+              <Image
+                src={placeholderImage}
+                alt={product.name ?? "Product Image"}
+                width={540}
+                height={540}
+                quality={85}
+                layout="responsive"
+              />
+            )
+          }
+        </a>
+      </Link>
     )
 
 }
